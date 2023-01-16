5f29a0dddc4212af7f2f8ac1b70358d60c0081a1
marcintkasper <marcin.t.kasper@gmail.com>
2023-01-16 09:55:05 -0500

    Created build script and initial page

--- README.md
+++ README.md
@@ -20,3 +20,13 @@ I create a static page every day for 123 days.
 * Become a better developer
 * Stop being a perfectionist
 * Get into a work routine
+
+## About this repository
+
+This is a node environment created with yarn. I am using "yarn run" scripts to build the site structure on command (first time!). 
+
+The `/src` folder contains the source code for the sites.
+
+The `/build` folder contains the resulting site for Github Pages.
+
+The `/scripts` folder contains the yarn build script(s).
---
+++ scripts/build-from-source.js
@@ -0,0 +1,88 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+// This is a quick and dirty script. Don't use it as is, please. No guarantees.
+
+const getPageDirectories = async () => {
+    const result = await fs.readdir(path.join("src", "pages"));
+    return result;
+}
+
+const getExistingPageDirectories = async () => {
+    const allFiles = await fs.readdir(path.join("build"));
+    result = allFiles.filter((e) => e.includes("day"))
+    return result;
+}
+
+const getPageInfo = async (pageDir) => {
+    const rawJson = await fs.readFile(path.join("src", "pages", pageDir, "info.json"));
+    const object = JSON.parse(rawJson);
+    const result = {path:pageDir, ...object};
+    return result;
+
+}
+
+const parseHomeHTML = async () => {
+    const homeHtml = await fs.readFile(path.join("src", "home", "index.html"));
+    const result = homeHtml.toString()
+    return result;
+}
+
+const genHomePageWithLinks = async () => {
+    
+    //This function finds the table in the home page and replaces it with a table of links
+
+    const pages = await getPageDirectories();
+    const pageInfoArray = await Promise.all(pages.map(async (e) => await getPageInfo(e)));
+
+    const tableStart = `<table><thead><tr><th>Day</th><th>Title</th><th>Prompt</th></tr></thead><tbody>`
+    const tableContent = pageInfoArray.map((e) => `<tr><td>Day ${e.day}</td><td><a href="/${e.path}">${e.title}</a></td><td>${e.prompt}</td></tr>`).join("");
+    const tableEnd = `</tbody></table>`;
+
+    const table = tableStart + tableContent + tableEnd;
+
+    const initialHomePage = await parseHomeHTML();
+    const splitHomePage = initialHomePage.split(/(<table>)(.|\t|\n|\r)*(<\/table>)/gm);
+    const newHomePage = splitHomePage[0] + table + splitHomePage[2];
+
+    return newHomePage;
+}
+
+const buildHomePage = async () => {
+    
+    await fs.copyFile(path.join("src", "home", "stylesheet.css"), path.join("build", "stylesheet.css"));
+    await fs.copyFile(path.join("src", "home", "script.js"), path.join("build", "script.js"));
+
+    const indexPageContent = await genHomePageWithLinks();
+    await fs.writeFile(path.join("build", "index.html"), indexPageContent);
+
+    console.log("Built home page");
+
+    
+}
+
+const copyOverNewPages = async () => {
+    const existingPageDirs = await getExistingPageDirectories();
+    const sourcePageDirs = await getPageDirectories();
+    const newPages = sourcePageDirs.filter((e) => !existingPageDirs.includes(e))
+    if (newPages.length < 1) {
+        console.log(Error("No new pages found."));
+        return;
+    }
+    
+    await Promise.all(newPages.map(async (e) => {
+        const fileList = await fs.readdir(path.join("src", "pages", e));
+        await fs.mkdir(path.join("build", e));
+        console.log("created " + e);
+        await Promise.all(fileList.map(async (f) => {
+            await fs.copyFile(path.join("src", "pages", e, f), path.join("build", e, f));
+            console.log("created " + e + "/" + f);
+        }))
+    }))
+
+    console.log("done")
+
+}
+
+copyOverNewPages();
+buildHomePage();
